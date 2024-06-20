5fa48ced2a9bf6ab47b904b3414b8738a4536f66
Spurkus <89559431+Spurkus@users.noreply.github.com>
2024-06-20 18:59:15 +1000

    Added loading

---
+++ src/components/Loading.tsx
@@ -0,0 +1,11 @@
+const Loading = () => {
+  return (
+    <div className="items-center self-center rounded-xl text-base">
+      <div className="flex">
+        <span className="loading loading-spinner h-12 w-12"></span>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
