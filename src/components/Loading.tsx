45d0f305f3b3684b47d6f0a07840617c81fbfb77
Spurkus <89559431+Spurkus@users.noreply.github.com>
2024-06-24 14:46:18 +1000

    Added custom height and width

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,8 +1,8 @@
-const Loading = () => {
+const Loading = ({ height = 12, width = 12 }: { height?: number; width?: number }) => {
   return (
     <div className="items-center self-center rounded-xl text-base">
       <div className="flex">
-        <span className="loading loading-spinner h-12 w-12"></span>
+        <span className={`loading loading-spinner h-${height} w-${width}`}></span>
       </div>
     </div>
   );
