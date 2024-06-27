2bf0950b9e9a2bd077dbc1f16b5d25bcf8077673
Spurkus <89559431+Spurkus@users.noreply.github.com>
2024-06-27 12:13:05 +1000

    Moved LINK_REGEX from User Profile to General

--- src/constants.ts
+++ src/constants.ts
@@ -3,13 +3,15 @@ export const MEGABYTES = 1048 * 1048;
 export const DEFAULT_IMAGE_SIZE = 1000;
 export const MAXIMUM_IMAGE_SIZE = 5 * MEGABYTES;
 
+// General Regex
+export const LINK_REGEX =
+  /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/;
+
 // User Profile Regex
 export const USERNAME_REGEX = /^[a-z]{2,15}$/;
 export const DISPLAY_NAME_REGEX = /^[A-Za-z0-9À-ÖØ-öø-ÿ'-. @&#:_]{2,15}$/;
 export const BIO_REGEX = /^[A-Za-z0-9À-ÖØ-öø-ÿ'-.?!@#$%^&*()_+=\[\]{}|\\;:"<>,/ \n]{0,150}$/;
 export const PRONOUNS_REGEX = /^[A-Za-zÀ-ÖØ-öø-ÿ]{1,6}$/;
-export const LINK_REGEX =
-  /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/;
 
 // User Profile Constants
 export const DEFAULT_PROFILE_PICTURE = process.env.NEXT_PUBLIC_DEFAULT_PROFILE_PICTURE as string;
