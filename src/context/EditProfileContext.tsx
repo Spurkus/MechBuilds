76df20db895f8302218cb6b2ef36405c09dc8da3
Spurkus <89559431+Spurkus@users.noreply.github.com>
2024-06-28 15:43:32 +1000

    Fixed grammatical error

--- src/context/EditProfileContext.tsx
+++ src/context/EditProfileContext.tsx
@@ -288,7 +288,7 @@ export const EditProfileContextProvider = ({
 export const useEditProfileContext = () => {
   const context = useContext(EditProfileContext);
   if (!context) {
-    throw new Error("useEditProfileContext must be used within a EditProfileContextProvider");
+    throw new Error("useEditProfileContext must be used within an EditProfileContextProvider");
   }
   return context;
 };
