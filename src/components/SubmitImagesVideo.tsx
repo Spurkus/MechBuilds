bad7dfeef7b9f7352bff01351387058603a56b95
Spurkus <89559431+Spurkus@users.noreply.github.com>
2024-07-06 21:29:12 +1000

    Added submitting video and previewing video

--- src/components/SubmitImagesVideo.tsx
+++ src/components/SubmitImagesVideo.tsx
@@ -4,8 +4,10 @@ import {
   DEFAULT_KEYBOARD_IMAGE_WIDTH,
   DEFAULT_KEYBOARD_IMAGE_HEIGHT,
   MAXIMUM_KEYBOARD_IMAGE_SIZE,
+  MAXIMUM_KEYBOARD_VIDEO_SIZE,
 } from "@/src/constants";
 import NextImage from "next/image";
+import ReactPlayer from "react-player";
 import { useGlobalModalContext } from "../context/GlobalModal";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -21,44 +23,95 @@ const SubmitImagesVideo = () => {
   const [isHovering, setIsHovering] = useState(false);
   const [index, setIndex] = useState(0);
   const [imageVideoList, setImageVideoList] = useState<File[]>([]);
-  const [imageURL, setImageURL] = useState("");
+  const [mediaURL, setMediaURL] = useState("");
   const isUploaded = useMemo(() => imageVideoList.length !== 0, [imageVideoList]);
   const firstIndex = useMemo(() => index === 0, [index]);
   const lastIndex = useMemo(() => index === imageVideoList.length - 1, [index, imageVideoList]);
+  const isCurrentMediaVideo = useMemo(
+    () => imageVideoList[index]?.type.startsWith("video/"),
+    [imageVideoList, index],
+  );
 
   const moveLeft = () => {
     if (firstIndex) return;
-    setImageURL("");
-    URL.revokeObjectURL(imageURL);
-    setImageURL(URL.createObjectURL(imageVideoList[index - 1]));
+    setMediaURL("");
+    URL.revokeObjectURL(mediaURL);
+    setMediaURL(URL.createObjectURL(imageVideoList[index - 1]));
     setIndex(index - 1);
   };
 
   const moveRight = () => {
     if (lastIndex) return;
-    setImageURL("");
-    URL.revokeObjectURL(imageURL);
-    setImageURL(URL.createObjectURL(imageVideoList[index + 1]));
+    setMediaURL("");
+    URL.revokeObjectURL(mediaURL);
+    setMediaURL(URL.createObjectURL(imageVideoList[index + 1]));
     setIndex(index + 1);
   };
 
-  const removeElement = (currentIndex: number) => {
+  const removeElement = () => {
     if (imageVideoList.length === 1) {
+      URL.revokeObjectURL(mediaURL);
       setImageVideoList([]);
-      setImageURL("");
-      URL.revokeObjectURL(imageURL);
-    } else if (currentIndex === 0) {
-      setImageURL("");
-      URL.revokeObjectURL(imageURL);
+      setMediaURL("");
       setIndex(0);
-      setImageURL(URL.createObjectURL(imageVideoList[1]));
-      setImageVideoList(imageVideoList.slice(1));
     } else {
-      setImageVideoList(imageVideoList.filter((_, i) => i !== currentIndex));
-      setImageURL("");
-      URL.revokeObjectURL(imageURL);
-      setIndex(currentIndex - 1);
-      setImageURL(URL.createObjectURL(imageVideoList[currentIndex]));
+      const newList = imageVideoList.filter((_, indexList) => indexList !== index);
+      const newIndex = index === 0 ? 0 : index >= newList.length ? newList.length - 1 : index;
+      URL.revokeObjectURL(mediaURL);
+      setImageVideoList(newList);
+      setMediaURL(URL.createObjectURL(newList[newIndex]));
+      setIndex(newIndex);
+    }
+  };
+
+  const handleImage = (file: File) => {
+    if (file.size > MAXIMUM_KEYBOARD_IMAGE_SIZE)
+      return handleModal("Uploading Image", "Image must be smaller than 10MB", "error");
+
+    // Image validation
+    const mediaURL = URL.createObjectURL(file);
+    const img = new Image();
+    img.onload = () => {
+      // Image must be bigger than 250x250px
+      if (img.width < 250 || img.height < 250) {
+        URL.revokeObjectURL(mediaURL); // Clean up
+        return handleModal("Uploading Image", "Image size must be bigger than 250x250px", "error");
+      }
+
+      URL.revokeObjectURL(mediaURL); // Clean up
+      setImageVideoList((prevList) => [...prevList, file]);
+      if (!isUploaded) setMediaURL(URL.createObjectURL(file));
+    };
+
+    // Check if the image is valid
+    img.onerror = () => {
+      URL.revokeObjectURL(mediaURL); // Clean up
+      return handleModal("Uploading Image", "Could not load the image", "error");
+    };
+
+    img.src = mediaURL;
+  };
+
+  const handleVideo = (file: File) => {
+    if (file.size > MAXIMUM_KEYBOARD_VIDEO_SIZE)
+      return handleModal("Uploading Video", "Video must be smaller than 30MB", "error");
+
+    setImageVideoList((prevList) => [...prevList, file]);
+    if (!isUploaded) setMediaURL(URL.createObjectURL(file));
+  };
+
+  const addFile = (file: File | undefined) => {
+    if (!file) {
+      handleModal("Uploading Image/Video", "No file selected", "error");
+      return;
+    }
+
+    if (file.type.startsWith("image/")) {
+      handleImage(file);
+    } else if (file.type.startsWith("video/")) {
+      handleVideo(file);
+    } else {
+      handleModal("Uploading Image/Video", "Invalid file type", "error");
     }
   };
 
@@ -72,6 +125,7 @@ const SubmitImagesVideo = () => {
           className="hidden"
           onChange={(e) => {
             addFile(e.target.files?.[0]);
+            e.target.value = ""; // Reset the input value
           }}
           {...props}
         />
@@ -82,48 +136,6 @@ const SubmitImagesVideo = () => {
   InputImageVideo.displayName = "InputImageVideo";
   const inputRef = useRef<HTMLInputElement>(null);
 
-  const handleImage = (file: File) => {
-    if (file.size > MAXIMUM_KEYBOARD_IMAGE_SIZE)
-      return handleModal("Uploading Image", "Image must be smaller than 10MB", "error");
-
-    // Image validation
-    const imageURL = URL.createObjectURL(file);
-    const img = new Image();
-    img.onload = () => {
-      // Image must be bigger than 250x250px
-      if (img.width < 250 || img.height < 250) {
-        URL.revokeObjectURL(imageURL); // Clean up
-        return handleModal("Uploading Image", "Image size must be bigger than 250x250px", "error");
-      }
-
-      URL.revokeObjectURL(imageURL); // Clean up
-      setImageVideoList((prevList) => [...prevList, file]);
-      if (!isUploaded) setImageURL(URL.createObjectURL(file));
-    };
-
-    // Check if the image is valid
-    img.onerror = () => {
-      URL.revokeObjectURL(imageURL); // Clean up
-      return handleModal("Uploading Image", "Could not load the image", "error");
-    };
-
-    img.src = imageURL;
-  };
-
-  const addFile = (file: File | undefined) => {
-    if (!file) {
-      handleModal("Uploading Image/Video", "No file selected", "error");
-      return;
-    }
-
-    if (file.type.includes("image")) {
-      handleImage(file);
-    } else if (file.type.includes("video")) {
-    } else {
-      handleModal("Uploading Image/Video", "Invalid file type", "error");
-    }
-  };
-
   return (
     <div
       className="avatar mask flex w-full grow self-center py-1"
@@ -131,82 +143,97 @@ const SubmitImagesVideo = () => {
       onMouseLeave={() => setIsHovering(false)}
     >
       {isUploaded && isHovering && (
-        <div className="absolute z-20 flex h-36 w-full grow p-3">
-          <div className="flex w-full grow flex-row justify-between">
-            <button
-              className="btn btn-sm flex rounded-2xl border-0 bg-gray-200 bg-opacity-10 hover:bg-gray-200 hover:bg-opacity-15"
-              onClick={() => inputRef.current?.click()}
-            >
-              <FontAwesomeIcon icon={faPhotoFilm} className="h-5 w-5 text-white" />
-              <span className="mt-0.5 font-bold">Add</span>
-            </button>
-            <button
-              className="btn btn-sm flex rounded-2xl border-0 bg-gray-200 bg-opacity-10 hover:bg-gray-200 hover:bg-opacity-15"
-              onClick={() => removeElement(index)}
-            >
-              <FontAwesomeIcon icon={faTrashAlt} className="h-5 w-5 text-error" />
-            </button>
-            <InputImageVideo ref={inputRef} />
-          </div>
-        </div>
+        <>
+          <button
+            className="btn btn-sm absolute left-3 top-3.5 z-10 flex rounded-2xl border-0 bg-gray-200 bg-opacity-10 hover:bg-gray-200 hover:bg-opacity-15"
+            onClick={() => inputRef.current?.click()}
+          >
+            <FontAwesomeIcon icon={faPhotoFilm} className="h-5 w-5 text-white" />
+            <span className="mt-0.5 font-bold">Add</span>
+          </button>
+          <button
+            className="btn btn-sm absolute right-3 top-3.5 z-10 flex rounded-2xl border-0 bg-gray-200 bg-opacity-10 hover:bg-gray-200 hover:bg-opacity-15"
+            onClick={removeElement}
+          >
+            <FontAwesomeIcon icon={faTrashAlt} className="h-5 w-5 text-error" />
+          </button>
+        </>
       )}
       {isUploaded && (
-        <div className="absolute z-10 flex h-full w-full grow p-3">
-          <div className="flex h-full w-full grow flex-row items-center">
-            {!firstIndex && (
-              <button
-                className="btn btn-circle btn-sm flex rounded-2xl border-0 bg-gray-200 bg-opacity-10 hover:bg-gray-200 hover:bg-opacity-15"
-                onClick={moveLeft}
-              >
-                <FontAwesomeIcon icon={faChevronLeft} className="h-5 w-5 text-white" />
-              </button>
-            )}
-            {!lastIndex && (
-              <div className="flex w-full grow justify-end">
-                <button
-                  className="btn btn-circle btn-sm flex rounded-2xl border-0 bg-gray-200 bg-opacity-10 hover:bg-gray-200 hover:bg-opacity-15"
-                  onClick={moveRight}
-                >
-                  <FontAwesomeIcon icon={faChevronRight} className="h-5 w-5 text-white" />
-                </button>
-              </div>
-            )}
-          </div>
-        </div>
+        <>
+          {!firstIndex && (
+            <button
+              className="btn btn-circle btn-sm absolute left-2 top-32 z-20 flex rounded-2xl border-0 bg-gray-200 bg-opacity-10 hover:bg-gray-200 hover:bg-opacity-15"
+              onClick={moveLeft}
+            >
+              <FontAwesomeIcon icon={faChevronLeft} className="h-5 w-5 text-white" />
+            </button>
+          )}
+          {!lastIndex && (
+            <button
+              className="btn btn-circle btn-sm absolute right-2 top-32 z-20 flex rounded-2xl border-0 bg-gray-200 bg-opacity-10 hover:bg-gray-200 hover:bg-opacity-15"
+              onClick={moveRight}
+            >
+              <FontAwesomeIcon icon={faChevronRight} className="h-5 w-5 text-white" />
+            </button>
+          )}
+        </>
       )}
       <label className={`relative w-full grow ${!isUploaded && "cursor-pointer"}`}>
         {isHovering && !isUploaded && (
-          <div className="absolute inset-0 flex items-center justify-center rounded-[1.5rem] bg-black bg-opacity-60">
-            <InputImageVideo />
+          <button
+            className="absolute inset-0 flex items-center justify-center rounded-[1.5rem] bg-black bg-opacity-60"
+            onClick={() => inputRef.current?.click()}
+          >
             <span className="text font-bold text-white">Add Images or Videos</span>
-          </div>
+          </button>
         )}
         {isUploaded && imageVideoList.length !== 1 && (
-          <>
-            <div className="absolute inset-0 flex items-end justify-center">
-              <div className="badge badge-neutral mb-2 space-x-1.5">
-                {imageVideoList.map((_, indexList) => (
-                  <FontAwesomeIcon
-                    key={indexList}
-                    icon={faCircle}
-                    className={`h-1.5 w-1.5 ${indexList !== index && "text-gray-500"}`}
-                  />
-                ))}
-              </div>
-            </div>
-          </>
+          <div className="badge badge-neutral absolute bottom-0 left-0 right-0 z-20 mx-auto mb-2 space-x-1.5">
+            {imageVideoList.map((_, indexList) => (
+              <FontAwesomeIcon
+                key={indexList}
+                icon={faCircle}
+                className={`h-1.5 w-1.5 ${indexList !== index && "text-gray-500"}`}
+              />
+            ))}
+          </div>
         )}
-        <NextImage
-          src={isUploaded ? imageURL : DEFAULT_KEYBOARD_IMAGE}
-          fill={false}
-          alt="Profile"
-          className="aspect-video rounded-[1.5rem] bg-gradient-to-b from-zinc-950 to-zinc-900"
-          style={{ objectFit: "contain" }}
-          width={DEFAULT_KEYBOARD_IMAGE_WIDTH}
-          height={DEFAULT_KEYBOARD_IMAGE_HEIGHT}
-          quality={100}
-        />
+        {isUploaded ? (
+          isCurrentMediaVideo ? (
+            <ReactPlayer
+              url={mediaURL}
+              controls
+              className="aspect-video rounded-[1.5rem] bg-gradient-to-b from-zinc-950 to-zinc-900"
+              width="100%"
+              height="100%"
+              style={{ objectFit: "contain" }}
+            />
+          ) : (
+            <NextImage
+              src={mediaURL}
+              fill={false}
+              alt="Profile"
+              className="aspect-video rounded-[1.5rem] bg-gradient-to-b from-zinc-950 to-zinc-900"
+              style={{ objectFit: "contain" }}
+              width={DEFAULT_KEYBOARD_IMAGE_WIDTH}
+              height={DEFAULT_KEYBOARD_IMAGE_HEIGHT}
+              quality={100}
+            />
+          )
+        ) : (
+          <NextImage
+            src={DEFAULT_KEYBOARD_IMAGE}
+            fill={false}
+            alt="Profile"
+            className="aspect-video rounded-[1.5rem] bg-gradient-to-b from-zinc-950 to-zinc-900"
+            width={DEFAULT_KEYBOARD_IMAGE_WIDTH}
+            height={DEFAULT_KEYBOARD_IMAGE_HEIGHT}
+            quality={100}
+          />
+        )}
       </label>
+      <InputImageVideo ref={inputRef} />
     </div>
   );
 };
