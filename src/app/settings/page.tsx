a1e5260c88dfc775ce7911674457b5f68d5e9dbb
Spurkus <89559431+Spurkus@users.noreply.github.com>
2024-07-08 22:12:05 +1000

    Fixed layout

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { useState } from "react";
+import classNames from "classnames";
 import NeedsAuthentication from "@/src/context/NeedsAuthentication";
 import ProfileDetails from "@/src/components/ProfileDetails/ProfileDetails";
 import AccountSettings from "./AccountSettings";
@@ -29,22 +30,27 @@ const Settings = () => {
   return (
     <NeedsAuthentication>
       <div className="flex w-full">
-        <div className="w-[23%] p-2">
+        <div className="w-[15rem] p-2">
           <SettingsMenu setSettings={setSettings} />
         </div>
-        <div className="flex w-[77%] grow">
-          <div className={`flex grow min-w-[${settings === "Account" ? "60" : "100"}%] p-2`}>
-            <div className="flex grow flex-col space-y-2 rounded-[2rem] bg-base-300 p-6">
-              {settings === "Account" && <AccountSettings />}
-              {settings === "Billing and Plans" && <BillingAndPlansSettings />}
-            </div>
-          </div>
-          {settings === "Account" && (
-            <div className="w-[40%] p-2">
-              <ProfileDetails />
-            </div>
+        <div
+          className={classNames(
+            "flex",
+            "grow",
+            "p-2",
+            settings === "Account" ? "w-[25rem]" : "w-[45rem]",
           )}
+        >
+          <div className="flex grow flex-col space-y-2 rounded-[2rem] bg-base-300 p-6">
+            {settings === "Account" && <AccountSettings />}
+            {settings === "Billing and Plans" && <BillingAndPlansSettings />}
+          </div>
         </div>
+        {settings === "Account" && (
+          <div className="w-[17.5rem] p-2">
+            <ProfileDetails />
+          </div>
+        )}
       </div>
     </NeedsAuthentication>
   );
