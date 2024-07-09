bae6bf18d2f9bf229c1d14652d7bdbd908232d83
Spurkus <89559431+Spurkus@users.noreply.github.com>
2024-07-09 10:25:34 +1000

    Changed layout for aesthetic

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -6,12 +6,12 @@ import DisplayUserKeyboards from "./DisplayUserKeyboards";
 const Profile = () => {
   return (
     <NeedsAuthentication>
-      <div className="flex w-full space-x-6">
-        <div className="w-[28%] p-2">
+      <div className="flex w-full justify-center space-x-6">
+        <div className="w-[20rem] p-2">
           <ProfileDetails />
         </div>
-        <div className="grow space-y-2">
-          <div className="mt-2 flex w-full justify-between">
+        <div className="space-y-2">
+          <div className="mt-2 flex w-[40rem] justify-between">
             <h1 className="font-regular font-clashgrotesk text-5xl">Profile</h1>
             <AddKeyboardButton />
           </div>
