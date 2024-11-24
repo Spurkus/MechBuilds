5598457163abc8ef7f3ac65aa66c369dd28eb43f
Spurk <oscartang.dev@gmail.com>
2024-11-24 17:50:18 +1000

    fix: font awesome icons over sized on refresh bug fixed with solution from FortAwesome/react-fontawesome#134

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,13 @@ import Footer from "@/src/components/General/Footer";
 import { GlobalModalContextProvider } from "@/src/context/GlobalModal";
 import { AuthContextProvider } from "@/src/context/Authentication";
 
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+import "@fortawesome/fontawesome-svg-core/styles.css";
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; /* eslint-disable import/first */
+
 export const metadata: Metadata = {
   title: "MechBuilds",
   description: "A keyboard management app for custom mechanical keyboard enthusiasts :3",
