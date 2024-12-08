d2bc6c5ae8ca644cae2a27dc5a7938f361e2eb91
Spurk <oscartang.dev@gmail.com>
2024-12-08 17:45:19 +1100

    feat: slide transition to keyboard media

--- src/components/General/DisplayImageVideo.tsx
+++ src/components/General/DisplayImageVideo.tsx
@@ -17,7 +17,6 @@ const DisplayImageVideo = ({ index, setIndex, imageVideoList, isMediaVideo }: Di
 
   const firstIndex = useMemo(() => index === 0, [index]);
   const lastIndex = useMemo(() => index === imageVideoList.length - 1, [index, imageVideoList]);
-  const isCurrentMediaVideo = useMemo(() => isMediaVideo[index], [isMediaVideo, index]);
 
   const moveLeft = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
@@ -51,7 +50,7 @@ const DisplayImageVideo = ({ index, setIndex, imageVideoList, isMediaVideo }: Di
           <FontAwesomeIcon icon={faChevronRight} className="h-5 w-5 text-white" />
         </button>
       )}
-      <label className="relative w-full grow cursor-pointer">
+      <label className="relative w-full grow cursor-pointer overflow-clip rounded-2xl">
         {imageVideoList.length !== 1 && (
           <div className="badge badge-neutral absolute bottom-0 left-0 right-0 z-20 mx-auto mb-2 space-x-1.5">
             {imageVideoList.map((_, indexList) => (
@@ -63,28 +62,40 @@ const DisplayImageVideo = ({ index, setIndex, imageVideoList, isMediaVideo }: Di
             ))}
           </div>
         )}
-        {isCurrentMediaVideo ? (
-          <ReactPlayer
-            url={imageVideoList[index]}
-            controls
-            pip={false}
-            className="aspect-video rounded-[1rem] bg-gradient-to-b from-zinc-950 to-zinc-900"
-            width="100%"
-            height="100%"
-            style={{ objectFit: "contain", overflow: "hidden" }}
-          />
-        ) : (
-          <NextImage
-            src={imageVideoList[index]}
-            fill={false}
-            alt="Profile"
-            className="aspect-video rounded-[1rem] bg-gradient-to-b from-zinc-950 to-zinc-900"
-            style={{ objectFit: "contain" }}
-            width={DEFAULT_KEYBOARD_IMAGE_WIDTH}
-            height={DEFAULT_KEYBOARD_IMAGE_HEIGHT}
-            quality={100}
-          />
-        )}
+        <div
+          className={`flex transition-transform duration-500`}
+          style={{
+            transform: `translateX(-${index * (100 / 2 ** (imageVideoList.length - 1))}%)`,
+            width: `${imageVideoList.length * 100.1}%`,
+          }}
+        >
+          {imageVideoList.map((source, indexList) =>
+            isMediaVideo[indexList] ? (
+              <ReactPlayer
+                key={indexList}
+                url={source}
+                controls
+                pip={false}
+                className="aspect-video bg-gradient-to-b from-zinc-950 to-zinc-900"
+                width="100%"
+                height="100%"
+                style={{ objectFit: "contain", overflow: "hidden" }}
+              />
+            ) : (
+              <NextImage
+                key={indexList}
+                src={source}
+                fill={false}
+                alt="Profile"
+                className="aspect-video bg-gradient-to-b from-zinc-950 to-zinc-900"
+                style={{ objectFit: "contain" }}
+                width={DEFAULT_KEYBOARD_IMAGE_WIDTH}
+                height={DEFAULT_KEYBOARD_IMAGE_HEIGHT}
+                quality={100}
+              />
+            ),
+          )}
+        </div>
       </label>
     </div>
   );
